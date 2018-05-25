4ec84aaaa9d86c8412f389c5fd27590c44c15684
Sean Grogan <sean.grogan@gmail.com>
2018-05-25 10:22:23 -0400

    Completed Program and added readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+#Shelter in Place
+
+[Image](Puzzlor_shelterinplace_JTv1.png)
+
+You have found yourself lost in the woods and you don't expect to be found for several months.  You'll need to build a camp so you are in close proximity to water, food, and firewood in order to survive the coming winter.
+
+Fortunately you have a map of the local area's resources (figure 1) which should help you decide where to set up your camp.  The available resources are water (indicated by the blue drop icon), firewood (indicated by the brown log icon), and food (indicated by the red rabbit icon).
+
+Choose any green location to set up your camp site with the goal of minimizing your daily travel distance to resources.  Unfortunately some resources require multiple trips per day to meet your needs.   Water requires three trips per day.  Firewood requires two trips per day, and food requires one trip per day.  You must always return back to camp after visiting any resource location.  Assume resources never run out. 
+
+For example, if you build your camp at location I9, you will have to travel a total of 32 units per day (12 units for water because it is 4 units round trip times 3 trips per day, 20 units for firewood because it is 10 units round trip times 2 trips per day, and 0 units for food because your camp occupies the same location).
+
+   
+Question:  At which location should you build your camp?
--- shelter_in_place.py
+++ shelter_in_place.py
@@ -1,5 +1,64 @@
+import sys
+
+
 def main():
-    pass
+    # Building Board
+    board = dict()
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    for i in letters:
+        board[i] = dict()
+        for j in range(10):
+            board[i][j+1] = dict()
+
+    # Inputting data
+    food = [['A', 6], ['E', 9], ['I', 9]]
+    water = [['F', 6], ['G', 9], ['H', 3]]
+    wood = [['B', 10], ['E', 1], ['I', 4]]
+    unavail = [['A', 10], ['A', 8], ['A', 5], ['A', 1],
+               ['F', 1],
+               ['G', 1], ['G', 10],
+               ['J', 1], ['J', 2], ['J', 6], ['J', 7], ['J', 10]]
+
+    # Unfortunately some resources require multiple trips per day to meet your needs.
+    # Water requires three trips per day.
+    # Firewood requires two trips per day,
+    # and food requires one trip per day.
+    # You must always return back to camp after visiting any resource location.
+    trip_food, trip_water, trip_wood = 1, 3, 2
+
+    best_location = ['K', 0, sys.maxsize]
+
+    # calculating distance
+    for i in letters:
+        for j in range(1, 11):
+            location = [i, j]
+            if location not in unavail:
+                d_food, d_wood, d_water = sys.maxsize, sys.maxsize, sys.maxsize
+                for k in food:
+                    d_food = min(d_food, dist(location, k))
+                for k in water:
+                    d_water = min(d_water, dist(location, k))
+                for k in wood:
+                    d_wood = min(d_wood, dist(location, k))
+                board[i][j]['food'] = d_food
+                board[i][j]['water'] = d_water
+                board[i][j]['wood'] = d_wood
+                board[i][j]['total'] = trip_food * d_food + trip_water * d_water + trip_wood * d_wood
+                if board[i][j]['total'] < best_location[2]:
+                    best_location = [i, j, board[i][j]['total']]
+
+    print(best_location)
+    print(board[best_location[0]][best_location[1]])
+
+
+def dist(loc1, loc2):
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    x1, y1 = loc1[0], loc1[1]
+    x2, y2 = loc2[0], loc2[1]
+    d_y = abs(y1 - y2)
+    d_x = abs(letters.index(x1) - letters.index(x2))
+    d = pow(pow(d_y, 2.0) + pow(d_x, 2.0), 0.5)
+    return d
 
 
 if __name__ == '__main__':
